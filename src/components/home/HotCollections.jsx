611a3ed918a4c5cb26ac00f0a8e4bfcf3c34655d
glf30 <greyson.frazier@gmail.com>
2023-10-05 19:32:34 -0400

    Make carousel responsive

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -16,6 +16,29 @@ const HotCollections = () => {
     speed: 500,
     slidesToShow: 4,
     slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1400,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   };
 
   useEffect(() => {
