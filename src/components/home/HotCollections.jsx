12346cf030960dbca3f73a6b01a2b5ad7548ca6e
glf30 <greyson.frazier@gmail.com>
2023-10-05 20:48:15 -0400

    Add skeleton loading states

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -22,23 +22,23 @@ const HotCollections = () => {
         settings: {
           slidesToShow: 3,
           slidesToScroll: 1,
-        }
+        },
       },
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 1,
-        }
+        },
       },
       {
         breakpoint: 768,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
   useEffect(() => {
@@ -65,9 +65,18 @@ const HotCollections = () => {
             </div>
           </div>
           <Slider {...settings}>
-            {hotCollections.map((collection, index) => (
-              <HotCollectionCard key={index} collection={collection} />
-            ))}
+            {hotCollections.length > 0
+              ? hotCollections.map((collection, index) => (
+                  <HotCollectionCard key={index} collection={collection} />
+                ))
+              : new Array(4)
+                  .fill(0)
+                  .map((collection, index) => (
+                    <SkeletonHotCollectionCard
+                      key={index}
+                      collection={collection}
+                    />
+                  ))}
           </Slider>
         </div>
       </div>
@@ -97,6 +106,44 @@ const HotCollectionCard = ({ collection }) => {
             <h4>{collection.title}</h4>
           </Link>
           <span>ERC-{collection.code}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const SkeletonHotCollectionCard = () => {
+  return (
+    <div className="hc-card">
+      <div className="nft_coll">
+        <div
+          className="skeleton-box lazy img-fluid"
+          style={{
+            width: "100%",
+            height: "190px",
+            borderRadius: "0.5rem 0.5rem 0px 0px",
+          }}
+        ></div>
+        <div className="nft_coll_pp">
+          <div>
+            <div
+              className="skeleton-box"
+              style={{ width: "50px", height: "50px", borderRadius: "50%" }}
+            ></div>
+          </div>
+          <i className="fa fa-check"></i>
+        </div>
+        <div className="nft_coll_info">
+          <div>
+            <h4
+              className="skeleton-box"
+              style={{ width: "96px", height: "16px", margin: "0px" }}
+            ></h4>
+          </div>
+          <span
+            className="skeleton-box"
+            style={{ width: "60px", height: "16px" }}
+          ></span>
         </div>
       </div>
     </div>
