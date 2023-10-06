c41d048eff76d8e8b3225683630c2ce62bdc5ba9
glf30 <greyson.frazier@gmail.com>
2023-10-06 18:51:11 -0400

    Add link to author info based on id

--- src/components/cards/ItemsCard.jsx
+++ src/components/cards/ItemsCard.jsx
@@ -36,7 +36,7 @@ export const ItemsCard = ({ item }) => {
       <div className="nft__item">
         <div className="author_list_pp">
           <Link
-            to="/author"
+            to={`/author/${item.authorId}`}
             data-bs-toggle="tooltip"
             data-bs-placement="top"
             title="Creator: Monica Lucas"
