2e9808763875542a87c93d1786f758e7fef2b392
glf30 <greyson.frazier@gmail.com>
2023-10-06 19:44:42 -0400

    Make Author Page Dynamic (#5)
    * Make display dynamic for author page
    
    * Add link to author info based on id
    
    * Add skeleton loading state
    
    * Implement follow button

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -51,13 +51,13 @@ const TopSellerCard = ({ seller }) => {
   return (
     <li>
       <div className="author_list_pp">
-        <Link to="/author">
+        <Link to={`/author/${seller.authorId}`}>
           <img className="lazy pp-author" src={seller.authorImage} alt="" />
           <i className="fa fa-check"></i>
         </Link>
       </div>
       <div className="author_list_info">
-        <Link to="/author">{seller.authorName}</Link>
+        <Link to={`/author/${seller.authorId}`}>{seller.authorName}</Link>
         <span>{seller.price} ETH</span>
       </div>
     </li>
--- src/pages/Author.jsx
+++ src/pages/Author.jsx
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AuthorBanner from "../images/author_banner.jpg";
 import AuthorItems from "../components/author/AuthorItems";
-import { Link } from "react-router-dom";
-import AuthorImage from "../images/author_thumbnail.jpg";
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
 
 const Author = () => {
+  const { id } = useParams();
+  const [items, setItems] = useState([]);
+  const [author, setAuthor] = useState({});
+  const [isFollowing, setIsFollowing] = useState(false);
+
+  useEffect(() => {
+    console.log(id);
+    const getItems = async () => {
+      const res = await axios.get(
+        `https://us-central1-nft-cloud-functions.cloudfunctions.net/authors?author=${id}`
+      );
+      const { data } = res;
+
+      const modifiedData = data.nftCollection.map((nft) => {
+        return { ...nft, ...data };
+      });
+
+      setItems([...modifiedData]);
+      setAuthor(data);
+    };
+
+    getItems();
+  }, []);
+
+  const handleFollow = () => {
+    setIsFollowing(!isFollowing);
+  }
+
   return (
     <div id="wrapper">
       <div className="no-bottom no-top" id="content">
@@ -25,29 +53,109 @@ const Author = () => {
                 <div className="d_profile de-flex">
                   <div className="de-flex-col">
                     <div className="profile_avatar">
-                      <img src={AuthorImage} alt="" />
+                      {!!author.authorImage ? (
+                        <img src={author.authorImage} alt="" />
+                      ) : (
+                        <div
+                          className="skeleton-box"
+                          style={{
+                            width: "150px",
+                            height: "150px",
+                            borderRadius: "50%",
+                          }}
+                        ></div>
+                      )}
 
                       <i className="fa fa-check"></i>
                       <div className="profile_name">
                         <h4>
-                          Monica Lucas
-                          <span className="profile_username">@monicaaaa</span>
-                          <span id="wallet" className="profile_wallet">
-                            UDHUHWudhwd78wdt7edb32uidbwyuidhg7wUHIFUHWewiqdj87dy7
+                          {!!author.authorName ? (
+                            <>{author.authorName}</>
+                          ) : (
+                            <div
+                              className="skeleton-box"
+                              style={{
+                                width: "200px",
+                                height: "25px",
+                              }}
+                            ></div>
+                          )}
+                          <span className="profile_username">
+                            {!!author.tag ? (
+                              <>@{author.tag}</>
+                            ) : (
+                              <div
+                                className="skeleton-box"
+                                style={{
+                                  width: "50%",
+                                  height: "20px",
+                                }}
+                              ></div>
+                            )}
                           </span>
-                          <button id="btn_copy" title="Copy Text">
-                            Copy
-                          </button>
+                          {!!author.address ? (
+                            <>
+                              <span id="wallet" className="profile_wallet">
+                                {author.address}
+                              </span>
+                              <button id="btn_copy" title="Copy Text">
+                                Copy
+                              </button>{" "}
+                            </>
+                          ) : (
+                            <div
+                              className="skeleton-box"
+                              style={{
+                                width: "200px",
+                                height: "20px",
+                              }}
+                            ></div>
+                          )}
                         </h4>
                       </div>
                     </div>
                   </div>
                   <div className="profile_follow de-flex">
                     <div className="de-flex-col">
-                      <div className="profile_follower">573 followers</div>
-                      <Link to="#" className="btn-main">
-                        Follow
-                      </Link>
+                      {author.followers ? (
+                        <>
+                          {!isFollowing ? (
+                            <>
+                              <div className="profile_follower">
+                                {author.followers} followers
+                              </div>
+                              <Link
+                                to="#"
+                                className="btn-main"
+                                onClick={handleFollow}
+                              >
+                                Follow
+                              </Link>
+                            </>
+                          ) : (
+                            <>
+                              <div className="profile_follower">
+                                {author.followers + 1} followers
+                              </div>
+                              <Link
+                                to="#"
+                                className="btn-main"
+                                onClick={handleFollow}
+                              >
+                                Unfollow
+                              </Link>
+                            </>
+                          )}
+                        </>
+                      ) : (
+                        <div
+                          className="skeleton-box"
+                          style={{
+                            width: "150px",
+                            height: "30px",
+                          }}
+                        ></div>
+                      )}
                     </div>
                   </div>
                 </div>
@@ -55,7 +163,7 @@ const Author = () => {
 
               <div className="col-md-12">
                 <div className="de_tab tab_simple">
-                  <AuthorItems />
+                  <AuthorItems items={items} />
                 </div>
               </div>
             </div>
