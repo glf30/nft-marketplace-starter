6c21eb1aa9dd9d7b9e0c0ec04319b7b376f3ddf2
glf30 <greyson.frazier@gmail.com>
2023-10-06 17:30:30 -0400

    Add Explore Items Page (#4)
    * Refactor NewItems card into component file
    
    * Refactor to generic name for card
    
    * fix name for refactor
    
    * Dynamic view for explore page (still showing all items)
    
    * Implement load more
    
    * Add filter path select for explore items

---
+++ src/components/cards/ItemsCard.jsx
@@ -0,0 +1,146 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+export const ItemsCard = ({ item }) => {
+  const [countdown, setCountdown] = useState({hours: 0, minutes: 0, seconds: 0});
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const newCountdown = calculateCountdown();
+      setCountdown(newCountdown);
+
+      if (newCountdown.total <= 0) {
+        clearInterval(interval);
+      }
+    }, 1000);
+
+    //used for cleanup when countdown finishes
+    return () => clearInterval(interval);
+  }, [countdown]);
+
+  const calculateCountdown = () => {
+    const time = new Date(item.expiryDate - Date.now());
+    const hours = time.getUTCHours();
+    const minutes = time.getUTCMinutes();
+    const seconds = time.getUTCSeconds();
+
+    return {
+      hours,
+      minutes,
+      seconds,
+    };
+  };
+
+  return (
+    <div className="new-item">
+      <div className="nft__item">
+        <div className="author_list_pp">
+          <Link
+            to="/author"
+            data-bs-toggle="tooltip"
+            data-bs-placement="top"
+            title="Creator: Monica Lucas"
+          >
+            <img className="lazy" src={item.authorImage} alt="" />
+            <i className="fa fa-check"></i>
+          </Link>
+        </div>
+        {!!item.expiryDate && (
+          <div className="de_countdown">{`${countdown.hours}h ${countdown.minutes}m ${countdown.seconds}s`}</div>
+        )}
+
+        <div className="nft__item_wrap">
+          <div className="nft__item_extra">
+            <div className="nft__item_buttons">
+              <button>Buy Now</button>
+              <div className="nft__item_share">
+                <h4>Share</h4>
+                <a href="" target="_blank" rel="noreferrer">
+                  <i className="fa fa-facebook fa-lg"></i>
+                </a>
+                <a href="" target="_blank" rel="noreferrer">
+                  <i className="fa fa-twitter fa-lg"></i>
+                </a>
+                <a href="">
+                  <i className="fa fa-envelope fa-lg"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <Link to="/item-details">
+            <img
+              src={item.nftImage}
+              className="lazy nft__item_preview"
+              alt=""
+            />
+          </Link>
+        </div>
+        <div className="nft__item_info">
+          <Link to="/item-details">
+            <h4>{item.title}</h4>
+          </Link>
+          <div className="nft__item_price">{item.price} ETH</div>
+          <div className="nft__item_like">
+            <i className="fa fa-heart"></i>
+            <span>{item.likes}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const SkeletonItemsCard = () => {
+  return (
+    <div className="new-item">
+      <div className="nft__item">
+        <div className="author_list_pp">
+          <div
+            className="skeleton-box"
+            style={{ width: "50px", height: "50px", borderRadius: "50%" }}
+          ></div>
+          <i className="fa fa-check"></i>
+        </div>
+        <div className="nft__item_wrap">
+          <div
+            className="skeleton-box"
+            style={{
+              width: "100%",
+              height: "101%",
+            }}
+          ></div>
+        </div>
+        <div className="nft__item_info">
+          <div>
+            <h4
+              className="skeleton-box"
+              style={{
+                margin: "8px 0px 0px 0px",
+                width: "60%",
+                height: "25px",
+              }}
+            ></h4>
+          </div>
+          <div
+            className="skeleton-box"
+            style={{
+              width: "40%",
+              height: "20px",
+            }}
+          ></div>
+        </div>
+        <div className="nft__item_like">
+          <div></div>
+          <div
+            className="skeleton-box"
+            style={{
+              width: "30px",
+              height: "15px",
+            }}
+          ></div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -1,77 +1,75 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+import axios from "axios";
+import { ItemsCard, SkeletonItemsCard } from "../cards/ItemsCard";
 
 const ExploreItems = () => {
+  const [items, setItems] = useState([]);
+  const [displaySize, setDisplaySize] = useState(8);
+  const [selectFilter, setSelectFilter] = useState("")
+
+  useEffect(() => {
+    const getItems = async () => {
+      const res = await axios.get(
+        `https://us-central1-nft-cloud-functions.cloudfunctions.net/explore${selectFilter}`
+      );
+      const { data } = res;
+
+      setItems([...data]);
+    };
+
+    getItems();
+  }, [selectFilter]);
+
+  const handleShowMore = () => {
+    setDisplaySize(displaySize + 4);
+  };
+
+  const handleSelect = (event) => {
+    setSelectFilter(`?filter=${event.target.value}`)
+    setItems([])
+  }
+
   return (
     <>
       <div>
-        <select id="filter-items" defaultValue="">
+        <select id="filter-items" defaultValue="" onChange={handleSelect}>
           <option value="">Default</option>
           <option value="price_low_to_high">Price, Low to High</option>
           <option value="price_high_to_low">Price, High to Low</option>
           <option value="likes_high_to_low">Most liked</option>
         </select>
       </div>
-      {new Array(8).fill(0).map((_, index) => (
-        <div
-          key={index}
-          className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
-          style={{ display: "block", backgroundSize: "cover" }}
-        >
-          <div className="nft__item">
-            <div className="author_list_pp">
-              <Link
-                to="/author"
-                data-bs-toggle="tooltip"
-                data-bs-placement="top"
-              >
-                <img className="lazy" src={AuthorImage} alt="" />
-                <i className="fa fa-check"></i>
-              </Link>
+      {items.length > 0
+        ? items.slice(0, displaySize).map((item, index) => (
+            <div
+              key={index}
+              className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
+              style={{ display: "block", backgroundSize: "cover" }}
+            >
+              <ItemsCard item={item} key={index} />
             </div>
-            <div className="de_countdown">5h 30m 32s</div>
-
-            <div className="nft__item_wrap">
-              <div className="nft__item_extra">
-                <div className="nft__item_buttons">
-                  <button>Buy Now</button>
-                  <div className="nft__item_share">
-                    <h4>Share</h4>
-                    <a href="" target="_blank" rel="noreferrer">
-                      <i className="fa fa-facebook fa-lg"></i>
-                    </a>
-                    <a href="" target="_blank" rel="noreferrer">
-                      <i className="fa fa-twitter fa-lg"></i>
-                    </a>
-                    <a href="">
-                      <i className="fa fa-envelope fa-lg"></i>
-                    </a>
-                  </div>
-                </div>
-              </div>
-              <Link to="/item-details">
-                <img src={nftImage} className="lazy nft__item_preview" alt="" />
-              </Link>
+          ))
+        : new Array(8).fill(0).map((_, index) => (
+            <div
+              key={index}
+              className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
+              style={{ display: "block", backgroundSize: "cover" }}
+            >
+              <SkeletonItemsCard key={index} />
             </div>
-            <div className="nft__item_info">
-              <Link to="/item-details">
-                <h4>Pinky Ocean</h4>
-              </Link>
-              <div className="nft__item_price">1.74 ETH</div>
-              <div className="nft__item_like">
-                <i className="fa fa-heart"></i>
-                <span>69</span>
-              </div>
-            </div>
-          </div>
-        </div>
-      ))}
+          ))}
       <div className="col-md-12 text-center">
-        <Link to="" id="loadmore" className="btn-main lead">
-          Load more
-        </Link>
+        {displaySize < items.length && (
+          <Link
+            to=""
+            id="loadmore"
+            className="btn-main lead"
+            onClick={handleShowMore}
+          >
+            Load more
+          </Link>
+        )}
       </div>
     </>
   );
