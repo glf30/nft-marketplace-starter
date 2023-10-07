f97d4d2c33811f646da60984ff8b41c13bc8da40
glf30 <greyson.frazier@gmail.com>
2023-10-06 21:06:45 -0400

    Add skeleton loading state

--- src/pages/ItemDetails.jsx
+++ src/pages/ItemDetails.jsx
@@ -2,13 +2,11 @@ import React, { useEffect, useState } from "react";
 import EthImage from "../images/ethereum.svg";
 import { Link, useParams } from "react-router-dom";
 import axios from "axios";
-import AuthorImage from "../images/author_thumbnail.jpg";
-import nftImage from "../images/nftImage.jpg";
 
 const ItemDetails = () => {
   const { id } = useParams();
   const [itemDetails, setItemDetails] = useState({});
-  
+
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
@@ -34,41 +32,113 @@ const ItemDetails = () => {
           <div className="container">
             <div className="row">
               <div className="col-md-6 text-center">
-                <img
-                  src={itemDetails.nftImage}
-                  className="img-fluid img-rounded mb-sm-30 nft-image"
-                  alt=""
-                />
+                {!!itemDetails.nftImage ? (
+                  <img
+                    src={itemDetails.nftImage}
+                    className="img-fluid img-rounded mb-sm-30 nft-image"
+                    alt=""
+                  />
+                ) : (
+                  <div
+                    className="skeleton-box"
+                    style={{
+                      width: "100%",
+                      height: "600px",
+                    }}
+                  ></div>
+                )}
               </div>
               <div className="col-md-6">
                 <div className="item_info">
-                  <h2>{itemDetails.title} #{itemDetails.tag}</h2>
+                  {!!itemDetails.title ? (
+                    <h2>
+                      {itemDetails.title} #{itemDetails.tag}
+                    </h2>
+                  ) : (
+                    <h2
+                      className="skeleton-box"
+                      style={{
+                        width: "60%",
+                        height: "40px",
+                      }}
+                    ></h2>
+                  )}
 
                   <div className="item_info_counts">
                     <div className="item_info_views">
-                      <i className="fa fa-eye"></i>
-                      {itemDetails.views}
+                      {!!itemDetails.views && (
+                        <>
+                          <i className="fa fa-eye"></i>
+                          {itemDetails.views}
+                        </>
+                      )}
                     </div>
                     <div className="item_info_like">
-                      <i className="fa fa-heart"></i>
-                      {itemDetails.likes}
+                      {!!itemDetails.likes ? (
+                        <>
+                          <i className="fa fa-heart"></i>
+                          {itemDetails.likes}
+                        </>
+                      ) : (
+                        <div
+                          style={{
+                            height: "40px",
+                          }}
+                        ></div>
+                      )}
                     </div>
                   </div>
-                  <p>
-                  {itemDetails.description}
-                  </p>
+                  <p>{itemDetails.description}</p>
+                  {!!itemDetails.description ? (
+                    <p>{itemDetails.description}</p>
+                  ) : (
+                    <p
+                      className="skeleton-box"
+                      style={{
+                        width: "100%",
+                        height: "100px",
+                      }}
+                    ></p>
+                  )}
                   <div className="d-flex flex-row">
                     <div className="mr40">
                       <h6>Owner</h6>
                       <div className="item_author">
                         <div className="author_list_pp">
-                          <Link to={`/author/${itemDetails.ownerId}`}>
-                            <img className="lazy" src={itemDetails.ownerImage} alt="" />
-                            <i className="fa fa-check"></i>
-                          </Link>
+                          {!!itemDetails.ownerId ? (
+                            <Link to={`/author/${itemDetails.ownerId}`}>
+                              <img
+                                className="lazy"
+                                src={itemDetails.ownerImage}
+                                alt=""
+                              />
+                              <i className="fa fa-check"></i>
+                            </Link>
+                          ) : (
+                            <div
+                              className="skeleton-box"
+                              style={{
+                                width: "50px",
+                                height: "50px",
+                                borderRadius: "50%",
+                              }}
+                            ></div>
+                          )}
                         </div>
                         <div className="author_list_info">
-                          <Link to={`/author/${itemDetails.ownerId}`}>{itemDetails.ownerName}</Link>
+                          {!!itemDetails.ownerId ? (
+                            <Link to={`/author/${itemDetails.ownerId}`}>
+                              {itemDetails.ownerName}
+                            </Link>
+                          ) : (
+                            <div
+                              className="skeleton-box"
+                              style={{
+                                width: "120px",
+                                height: "20px",
+                              }}
+                            ></div>
+                          )}
                         </div>
                       </div>
                     </div>
@@ -79,21 +149,62 @@ const ItemDetails = () => {
                       <h6>Creator</h6>
                       <div className="item_author">
                         <div className="author_list_pp">
-                          <Link to={`/author/${itemDetails.creatorId}`}>
-                            <img className="lazy" src={itemDetails.creatorImage} alt="" />
-                            <i className="fa fa-check"></i>
-                          </Link>
+                          {!!itemDetails.creatorId ? (
+                            <Link to={`/author/${itemDetails.creatorId}`}>
+                              <img
+                                className="lazy"
+                                src={itemDetails.creatorImage}
+                                alt=""
+                              />
+                              <i className="fa fa-check"></i>
+                            </Link>
+                          ) : (
+                            <div
+                              className="skeleton-box"
+                              style={{
+                                width: "50px",
+                                height: "50px",
+                                borderRadius: "50%",
+                              }}
+                            ></div>
+                          )}
                         </div>
                         <div className="author_list_info">
-                          <Link to={`/author/${itemDetails.creatorId}`}>{itemDetails.creatorName}</Link>
+                          {!!itemDetails.creatorId ? (
+                            <Link to={`/author/${itemDetails.creatorId}`}>
+                              {itemDetails.creatorName}
+                            </Link>
+                          ) : (
+                            <div
+                              className="skeleton-box"
+                              style={{
+                                width: "120px",
+                                height: "20px",
+                              }}
+                            ></div>
+                          )}
                         </div>
                       </div>
                     </div>
                     <div className="spacer-40"></div>
                     <h6>Price</h6>
                     <div className="nft-item-price">
-                      <img src={EthImage} alt="" />
-                      <span>{itemDetails.price}</span>
+                      {!!itemDetails.price ? (
+                        <>
+                          <img src={EthImage} alt="" />
+                          <span>{itemDetails.price}</span>
+                        </>
+                      ) : (
+                        <span>
+                          <div
+                            className="skeleton-box"
+                            style={{
+                              width: "60px",
+                              height: "20px",
+                            }}
+                          ></div>
+                        </span>
+                      )}
                     </div>
                   </div>
                 </div>
